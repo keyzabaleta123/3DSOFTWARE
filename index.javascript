6966abade48633e3881da0055496eeb912bf18d3
Keyly Zabaleta <zabaletakeyly@gmail.com>
2025-02-09 22:49:41 -0600

    Commit Inicial

--- index.javascript
+++
@@ -1,3 +0,0 @@
-document.getElementById("greetButton").addEventListener("click", function () {
-  alert("¡Hola, 3°SOFTWARE espero que esten disfrutando aprender a programar🎉");
-});
---
+++ script.js
@@ -0,0 +1,4 @@
+// Cuando el botón "Iniciar Ahora" es presionado, muestra una alerta
+document.getElementById("startButton").addEventListener("click", function() {
+  alert("¡Genial! Estás listo para empezar tu aventura en la programación. ¡Vamos allá!");
+});
